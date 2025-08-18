204505eb1c47f7aceff9d0b742951100cccda0a6
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-08-19 00:00:10 +0300

    Login path allow

--- src/main/java/com/kridan/split_net/config/AuthConfiguration.java
+++ src/main/java/com/kridan/split_net/config/AuthConfiguration.java
@@ -3,6 +3,7 @@ package com.kridan.split_net.config;
 import org.bouncycastle.crypto.generators.BCrypt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,18 +19,21 @@ import javax.crypto.spec.SecretKeySpec;
 public class AuthConfiguration {
 
     @Bean
+    @Order(1)
     public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
         // Применяем дефолтную конфигурацию OAuth2 Authorization Server
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
         // Включаем форму логина для авторизации пользователей
         http
+                .csrf(csrf -> csrf.disable())
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
