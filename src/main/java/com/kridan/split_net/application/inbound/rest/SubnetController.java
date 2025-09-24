edcd61e8ee347b4a641322ee80607e0045883aa8
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-09-24 21:09:53 +0300

    Subnet controller added

--- src/main/java/com/kridan/split_net/application/inbound/rest/SubnetController.java
+++ src/main/java/com/kridan/split_net/application/inbound/rest/SubnetController.java
@@ -1,13 +1,13 @@
 package com.kridan.split_net.application.inbound.rest;
 
+import com.kridan.split_net.application.inbound.rest.dto.CreateSubnetRequest;
 import com.kridan.split_net.domain.subnet.Subnet;
+import com.kridan.split_net.domain.subnet.usecases.CreateSubnetUseCase;
 import com.kridan.split_net.domain.subnet.usecases.GetSubnetsUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,13 +17,32 @@ import java.util.List;
 @Slf4j
 public class SubnetController {
 
+    private final CreateSubnetUseCase createSubnetUseCase;
     private final GetSubnetsUseCase getSubnetsUseCase;
 
     @GetMapping("/get-all")
     public ResponseEntity<?> getSubnets(){
         try {
+            log.debug("Call endpoint '/api/subnet/get-all'");
             List<Subnet> subnets = getSubnetsUseCase.getAll();
             return ResponseEntity.ok(subnets);
+        } catch (Exception e){
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().body("An error occurred");
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createSubnet(@RequestBody CreateSubnetRequest createSubnetRequest){
+        try {
+            log.debug("Call endpoint '/api/subnet/create'");
+            Subnet subnet = createSubnetUseCase.create(
+                    createSubnetRequest.getName(),
+                    createSubnetRequest.getDescription(),
+                    createSubnetRequest.getCidr()
+            );
+            log.debug("Subnet with CIDR: {} created", subnet.getCidr());
+            return ResponseEntity.ok("Subnet created successful");
         } catch (Exception e){
             log.error(e.getMessage());
             return ResponseEntity.internalServerError().body("An error occurred");
