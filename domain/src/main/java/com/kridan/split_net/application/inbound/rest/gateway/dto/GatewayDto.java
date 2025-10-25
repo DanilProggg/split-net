a062874bb47a0ecf126f3e476973f6b238ee869e
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-10-25 18:16:09 +0300

    Gateway service

---
+++ domain/src/main/java/com/kridan/split_net/application/inbound/rest/gateway/dto/GatewayDto.java
@@ -0,0 +1,23 @@
+package com.kridan.split_net.application.inbound.rest.gateway.dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class GatewayDto {
+    private Long gateway_id;
+    private String name;
+    private String wg_url;
+    private String publicKey;
+    private Long site_id;
+
+    public GatewayDto(Long gateway_id, String name, String wg_url, String publicKey, Long site_id) {
+        this.gateway_id = gateway_id;
+        this.name = name;
+        this.wg_url = wg_url;
+        this.publicKey = publicKey;
+        this.site_id = site_id;
+    }
+}
