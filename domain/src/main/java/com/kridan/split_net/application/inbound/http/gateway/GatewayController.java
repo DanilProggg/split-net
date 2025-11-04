91496a709b8f7af520f69ac83570fd2c96dbd087
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-11-04 16:57:56 +0300

    Temp IP address

--- domain/src/main/java/com/kridan/split_net/application/inbound/http/gateway/GatewayController.java
+++ domain/src/main/java/com/kridan/split_net/application/inbound/http/gateway/GatewayController.java
@@ -53,7 +53,7 @@ public class GatewayController {
                             gateway -> new GatewayDto(
                                     gateway.getId(),
                                     gateway.getName(),
-                                    gateway.getWg_url(),
+                                    gateway.getWgUrl(),
                                     gateway.getPublicKey(),
                                     gateway.getIpAddress(),
                                     gateway.getLastSeen(),
