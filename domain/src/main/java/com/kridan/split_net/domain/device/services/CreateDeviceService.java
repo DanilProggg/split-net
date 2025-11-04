0fc0c3b0cf4e289fa33db428ab15dd34e6b11851
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-11-04 16:46:29 +0300

    Temp IP address

--- domain/src/main/java/com/kridan/split_net/domain/device/services/CreateDeviceService.java
+++ domain/src/main/java/com/kridan/split_net/domain/device/services/CreateDeviceService.java
@@ -28,6 +28,8 @@ public class CreateDeviceService implements CreateDeviceUseCase {
                                                   String pubkey) throws IOException, InterruptedException {
         try {
 
+            String ipAddress = "100.64.1.1";
+
             //Make dhcp for IP
             Device device = deviceFactory.create(
                     findUserPort.findById(UUID.fromString(userId)),
