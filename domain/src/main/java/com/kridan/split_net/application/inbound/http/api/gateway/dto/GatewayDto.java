7045029a52828d0d4c18ac9a50cbfb9ada86bba0
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-11-11 21:00:00 +0300

    Gateway Status check

--- domain/src/main/java/com/kridan/split_net/application/inbound/http/api/gateway/dto/GatewayDto.java
+++ domain/src/main/java/com/kridan/split_net/application/inbound/http/api/gateway/dto/GatewayDto.java
@@ -1,6 +1,5 @@
 package com.kridan.split_net.application.inbound.http.api.gateway.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
@@ -8,13 +7,22 @@ import java.util.Date;
 
 @Data
 @Getter
-@AllArgsConstructor
 public class GatewayDto {
     private Long gateway_id;
     private String name;
     private String wg_url;
     private String publicKey;
     private String ipAddress;
-    private Date lastSeen;
+    private boolean isActive;
     private Long site_id;
+
+    public GatewayDto(Long gateway_id, String name, String wg_url, String publicKey, String ipAddress, Date lastSeen, Long site_id) {
+        this.gateway_id = gateway_id;
+        this.name = name;
+        this.wg_url = wg_url;
+        this.publicKey = publicKey;
+        this.ipAddress = ipAddress;
+        this.isActive = (System.currentTimeMillis() - lastSeen.getTime()) < 2 * 60 * 1000;
+        this.site_id = site_id;
+    }
 }
