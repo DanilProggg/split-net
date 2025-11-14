484d3efc4a2538b667021af66587d018e698e1ca
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-11-14 11:07:38 +0300

    Resource

--- domain/src/main/java/com/kridan/split_net/application/outbound/db/resource/SaveResourceAdapter.java
+++ domain/src/main/java/com/kridan/split_net/application/outbound/db/resource/SaveResourceAdapter.java
@@ -1,9 +1,19 @@
 package com.kridan.split_net.application.outbound.db.resource;
 
+import com.kridan.split_net.domain.resource.Resource;
+import com.kridan.split_net.domain.resource.ports.SaveResourcePort;
+import com.kridan.split_net.infrastructure.database.repository.resource.ResourceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class SaveResourceAdapter implements Fin{
+public class SaveResourceAdapter implements SaveResourcePort {
+
+    private final ResourceRepository resourceRepository;
+
+    @Override
+    public Resource create(Resource resource) {
+        return resourceRepository.save(resource);
+    }
 }
--- domain/src/main/java/com/kridan/split_net/domain/resource/ports/SaveResourcePort.java
+++ domain/src/main/java/com/kridan/split_net/domain/resource/ports/SaveResourcePort.java
@@ -3,5 +3,5 @@ package com.kridan.split_net.domain.resource.ports;
 import com.kridan.split_net.domain.resource.Resource;
 
 public interface SaveResourcePort {
-    boolean create(Resource resource);
+    Resource create(Resource resource);
 }
