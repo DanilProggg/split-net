58071941effb9b6845a0a7d828ebfe2369bfcf9b
DanilProggg <krivoboc.krivoboc@gmail.com>
2025-11-17 09:31:35 +0300

    Auth form

--- domain/src/main/java/com/kridan/split_net/application/inbound/http/api/user/AuthController.java
+++ domain/src/main/java/com/kridan/split_net/application/inbound/http/api/user/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final CreateUserUseCase createUserUseCase;
