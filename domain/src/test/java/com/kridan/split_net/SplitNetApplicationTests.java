7608378c67f2d6d6650aff26126794ad33a4cd51
DanilProggg <krivoboc.krivoboc@gmail.com>
2026-05-08 15:40:06 +0300

    Tests

--- domain/src/test/java/com/kridan/split_net/SplitNetApplicationTests.java
+++ domain/src/test/java/com/kridan/split_net/SplitNetApplicationTests.java
@@ -1,8 +1,10 @@
 package com.kridan.split_net;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class SplitNetApplicationTests {
 
